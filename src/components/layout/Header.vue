bc84e6b63a5b203240dc764444f5920f9f212132
hyun <enjoy.hyun@gmail.com>
2021-05-11 18:10:29 +0900

    바인딩 변수명 수정

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -13,7 +13,7 @@
     </div>
     <!--// personal menu -->
     <div class="flex flex-none items-center px-4 z-11">
-    <div v-on:click="isPersonalMenu = !isPersonalMenu, exceptionObj = true" v-click-outside="btnPersonalMenuOutSide" class="mr-4 text-gray-200 cursor-pointer"><i class="fas fa-user text-2xl"></i></div>
+    <div v-on:click="isPersonalMenu = !isPersonalMenu, isPerosnalMenuException = true" v-click-outside="btnPersonalMenuOutSide" class="mr-4 text-gray-200 cursor-pointer"><i class="fas fa-user text-2xl"></i></div>
     <div v-show="isPersonalMenu" v-click-outside="personalMenuClose" class="fixed top-14 right-2 w-40 rounded-sm bg-white z-20 border border-top-none border-solid border-gray-300">
       <ul class="w-full text-sm select-none">
         <li class="pl-2 py-2 cursor-pointer hover:bg-blue-400 hover:text-white"><i class="fas fa-user-circle"></i> Profile</li>
@@ -33,17 +33,17 @@ export default {
   data () {
     return {
       isPersonalMenu: false,
-      exceptionObj: false
+      isPerosnalMenuException: false
     }
   },
   methods: {
     personalMenuClose(event) {
-      if( this.exceptionObj == false ){
+      if( this.isPerosnalMenuException == false ){
         this.isPersonalMenu = false
       }
     },
     btnPersonalMenuOutSide() {
-      this.exceptionObj = false
+      this.isPerosnalMenuException = false
     }
   }
 };
